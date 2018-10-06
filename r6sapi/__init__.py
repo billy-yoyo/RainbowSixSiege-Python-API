61c97a09a8a45b51bab95977678b78df1315ea3b
Will Page <billyoyo@hotmail.co.uk>
2018-10-06 12:39:01 +0100

    update version number to 0.8.0

--- r6sapi/__init__.py
+++ r6sapi/__init__.py
@@ -4,7 +4,7 @@ __title__ = 'r6sapi'
 __author__ = 'billyoyo'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2016-2018 billyoyo'
-__version__ = '0.7.0'
+__version__ = '0.8.0'
 
 from .r6sapi import *
 
