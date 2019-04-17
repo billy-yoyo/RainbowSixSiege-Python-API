9860fdfd9a78aabd977eaa71b0a4ab4ed69e94d0
Will Page <billyoyo@hotmail.co.uk>
2019-04-17 20:15:30 +0100

    update copyright year and version number

--- r6sapi/__init__.py
+++ r6sapi/__init__.py
@@ -3,8 +3,8 @@
 __title__ = 'r6sapi'
 __author__ = 'billyoyo'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2016-2018 billyoyo'
-__version__ = '0.8.0'
+__copyright__ = 'Copyright 2016-2019 billyoyo'
+__version__ = '0.9.0'
 
 from .r6sapi import *
 
