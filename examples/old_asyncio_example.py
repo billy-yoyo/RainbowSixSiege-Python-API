922ecd99f2237a9177a28ba589293848b5c54f66
Jack <jackywathy24@gmail.com>
2021-01-02 20:20:17 +1100

    Revert "updated example/old_asyncio_example to use await / async syntax"
    This reverts commit a9f81d2456615ecedc8f6b8fbdabb9cfdd1d5e3e.

--- examples/old_asyncio_example.py
+++ examples/old_asyncio_example.py
@@ -1,14 +1,14 @@
 import asyncio
 import r6sapi as api
 
-
-async def run():
+@asyncio.coroutine
+def run():
     auth = api.Auth("email", "password")
-
-    player = await auth.get_player("billy_yoyo", api.Platforms.UPLAY)
-    operator = await player.get_operator("sledge")
+    
+    player = yield from auth.get_player("billy_yoyo", api.Platforms.UPLAY)
+    operator = yield from player.get_operator("sledge")
     print(operator.kills)
 
-    await auth.close()
+    yield from auth.close()
     
 asyncio.get_event_loop().run_until_complete(run())
